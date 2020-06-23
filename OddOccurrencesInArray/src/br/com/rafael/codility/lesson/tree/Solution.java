053d47c660bbbcf765b68906c1e371de81d6f33f
Rafael Alves <barbosa.alves@hotmail.com.br>
2020-06-23 13:26:14 -0400

    Update Solution.java

--- OddOccurrencesInArray/src/br/com/rafael/codility/lesson/tree/Solution.java
+++ OddOccurrencesInArray/src/br/com/rafael/codility/lesson/tree/Solution.java
@@ -1,45 +1,27 @@
 package br.com.rafael.codility.lesson.tree;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
 
-public class Solution {
-    
-    private int valor = -1;
-    private ArrayList<Integer> valores = new ArrayList<Integer>();
-    private ArrayList<Integer> A = new ArrayList<Integer>();
-    
-    public int solution(int[] A){
-        
-        int contador=0;
-        
-        if(A.length % 2 != 0){
-            
-            for(int i=0;i<A.length;i++){
-                this.A.add(A[i]);
-            }
-            
-            for(int i=0;i<this.A.size();i++){
-                if(!(valores.contains(this.A.get(i)))){
-                    valores.add(A[i]);
-                }
-            }
-            
-            for(int i=0;i<valores.size();i++){
-                for(int j=0;j<this.A.size();j++){
-                    if(valores.get(i).equals(this.A.get(j))){
-                        contador++;
-                    }
-                }
-                
-                if(contador % 2 != 0) {
-                    valor = valores.get(i);
-                }
-                
-                contador=0;
-            }
-            
-        }
-        
-        return valor;
+class Solution {
+    public int solution(int[] A) {
+        int[] noDuplicates = IntStream.of(A).distinct().toArray();
+		Map<Integer, Integer> counter = new HashMap<Integer, Integer>();
+
+		for (int value : noDuplicates) {
+			counter.put(value, 0);
+		}
+
+		for (int value : A) {
+			counter.put(value, counter.get(value) + 1);
+		}
+
+		for (Map.Entry<Integer, Integer> value : counter.entrySet()) {
+			if (value.getValue() % 2 != 0)
+				return value.getKey();
+		}
+
+		return 0;
     }
 }
