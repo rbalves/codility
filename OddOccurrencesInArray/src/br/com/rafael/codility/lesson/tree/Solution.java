d29aa700bc3f3524befc01bc2b7c828457910f0a
Rafael Alves <barbosa.alves@hotmail.com.br>
2020-06-23 13:27:02 -0400

    Update Solution.java

--- OddOccurrencesInArray/src/br/com/rafael/codility/lesson/tree/Solution.java
+++ OddOccurrencesInArray/src/br/com/rafael/codility/lesson/tree/Solution.java
@@ -6,22 +6,23 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] A) {
+	    
         int[] noDuplicates = IntStream.of(A).distinct().toArray();
-		Map<Integer, Integer> counter = new HashMap<Integer, Integer>();
+	Map<Integer, Integer> counter = new HashMap<Integer, Integer>();
 
-		for (int value : noDuplicates) {
-			counter.put(value, 0);
-		}
+	for (int value : noDuplicates) {
+		counter.put(value, 0);
+	}
 
-		for (int value : A) {
-			counter.put(value, counter.get(value) + 1);
-		}
+	for (int value : A) {
+		counter.put(value, counter.get(value) + 1);
+	}
 
-		for (Map.Entry<Integer, Integer> value : counter.entrySet()) {
-			if (value.getValue() % 2 != 0)
-				return value.getKey();
-		}
+	for (Map.Entry<Integer, Integer> value : counter.entrySet()) {
+		if (value.getValue() % 2 != 0)
+			return value.getKey();
+	}
 
-		return 0;
+	return 0;
     }
 }
